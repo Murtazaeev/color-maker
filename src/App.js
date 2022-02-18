1c2c72a04a9df4d21321d558dd398a379152fbe7
Temur Murtazaev <temur.murtazaeveurope@gmail.com>
2022-02-18 21:22:04 +0100

    started route section on my project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Switch, Route } from 'react-router-dom';
 import { generatePalette } from './colorHelpers';
 import Palette from './Palette';
 import seedColors from './seedColors';
@@ -8,12 +9,20 @@ class App extends Component {
   render() {
     
     return (
-      <div className="App">
-        <Palette palette={generatePalette(seedColors[4])} />
-      </div>
+      <Switch>
+        <Route exact path="/" render={ () =>  <h1>Palette list goes here</h1>} />
+          <Route exact path='/palette/:id' render={ () =>  <h1>Palette  individual</h1>}/>
+          
+     
+      </Switch>
+     
+      
+            //       <div className="App">
+            //   <Palette palette={generatePalette(seedColors[4])} />
+            // </div>
+      
     );
   }
   
 }
-
 export default App;
