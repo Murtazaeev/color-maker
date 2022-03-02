07b33aaab1606fba7d2c19f8ab3b38f082307a68
Temur Murtazaev <temur.murtazaeveurope@gmail.com>
2022-03-02 03:14:45 +0100

    refactoring css to jss xd

--- src/PaletteFooter.js
+++ src/PaletteFooter.js
@@ -1,13 +1,29 @@
+import { withStyles } from '@mui/styles';
 import React from 'react';
+
+const styles = {
+	paletteFooter: {
+		backgroundColor: 'white',
+		height: '5vh',
+		display: 'flex',
+		justifyContent: 'flex-end',
+		alignItems: 'center',
+		fontWeight: 'bold'
+	},
+	emoji: {
+		fontSize: '1.5rem',
+		margin: '0 1rem'
+	}
+};
 
 function PaletteFooter(props) {
 	const { paletteName, emoji } = props;
+	const { classes } = props;
 	return (
-		<footer className="Palette-footer">
+		<footer className={classes.paletteFooter}>
 			{paletteName}
-			<span className="emoji">{emoji}</span>
+			<span className={classes.emoji}>{emoji}</span>
 		</footer>
 	);
 }
-
-export default PaletteFooter;
+export default withStyles(styles)(PaletteFooter);
