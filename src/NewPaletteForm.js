1b2f00c4876cfba8d86d0e6507529fbb87f7b682
Temur Murtazaev <temur.murtazaeveurope@gmail.com>
2022-03-09 23:43:01 +0100

    styling palettenav started

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -18,23 +18,7 @@ const styles = (theme) => ({
 	root: {
 		display: 'flex'
 	},
-	appBar: {
-		transition: theme.transitions.create([ 'margin', 'width' ], {
-			easing: theme.transitions.easing.sharp,
-			duration: theme.transitions.duration.leavingScreen
-		})
-	},
-	appBarShift: {
-		width: `calc(100% - ${drawerWidth}px)`,
-		marginLeft: drawerWidth,
-		transition: theme.transitions.create([ 'margin', 'width' ], {
-			easing: theme.transitions.easing.easeOut,
-			duration: theme.transitions.duration.enteringScreen
-		})
-	},
-	menuButton: {
-		marginRight: theme.spacing(2)
-	},
+
 	hide: {
 		display: 'none'
 	},
@@ -225,7 +209,6 @@ class NewPaletteForm extends React.Component {
 			<div className={classes.root}>
 				<PaletteFormNav
 					open={open}
-					classes={classes}
 					palettes={palettes}
 					handleSubmit={this.handleSubmit}
 					handleDrawerOpen={this.handleDrawerOpen}
