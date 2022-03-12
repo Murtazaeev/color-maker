737e285771875f7d2b36fd1e9dee06450e29f5c3
Temur Murtazaev <temur.murtazaeveurope@gmail.com>
2022-03-12 14:39:56 +0100

    added bachground by SVG

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,11 +1,16 @@
 import sizes from './sizes';
+import bg from './bg.svg';
 export default {
 	root: {
-		backgroundColor: 'blue',
 		height: '100vh',
 		display: 'flex',
 		alignItems: 'flex-start',
-		justifyContent: 'center'
+		justifyContent: 'center',
+		// for background by SVG
+
+		backgroundColor: '#1e8feb',
+		backgroundImage: `url(${bg})`,
+		overflow: 'scroll'
 	},
 	container: {
 		width: '50%',
