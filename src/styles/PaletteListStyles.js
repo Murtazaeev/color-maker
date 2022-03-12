0adf97bc447bd92bc2634314d736ae9f142cba60
Temur Murtazaev <temur.murtazaeveurope@gmail.com>
2022-03-12 14:15:54 +0100

    responsible paletteList

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,3 +1,4 @@
+import sizes from './sizes';
 export default {
 	root: {
 		backgroundColor: 'blue',
@@ -11,7 +12,16 @@ export default {
 		display: 'flex',
 		alignItems: 'flex-start',
 		flexDirection: 'column',
-		flexWrap: 'wrap'
+		flexWrap: 'wrap',
+		[sizes.down('xl')]: {
+			width: '80%'
+		},
+		[sizes.down('xs')]: {
+			width: '75%'
+		}
+	},
+	heading: {
+		fontSize: '2rem'
 	},
 	nav: {
 		display: 'flex',
@@ -19,16 +29,23 @@ export default {
 		justifyContent: 'space-between',
 		alignItems: 'center',
 		color: 'white',
+		alignItems: 'center',
 		'& a': {
 			color: 'white'
-		},
-		alignItems: 'center'
+		}
 	},
 	palettes: {
 		boxSizing: 'border-box',
 		width: '100%',
 		display: 'grid',
 		gridTemplateColumns: 'repeat(3, 30%)',
-		gridGap: '5%'
+		gridGap: '2.5rem',
+		[sizes.down('md')]: {
+			gridTemplateColumns: 'repeat(2, 50%)'
+		},
+		[sizes.down('xs')]: {
+			gridTemplateColumns: 'repeat(1, 100%)',
+			gridGap: '1.4rem'
+		}
 	}
 };
